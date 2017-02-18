f0a11157339d87e0ecc76b0f4acaacf1edc11d10
Jaszczur <jaszczur20@interia.pl>
2017-02-18 16:00:03 +0100

    telegram integration - very simple UX

--- src/main/java/pl/jaszczur/bots/aqi/Main.java
+++ src/main/java/pl/jaszczur/bots/aqi/Main.java
@@ -13,28 +13,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Odpytujemy serwera");
         AirQualityApi airQualityApi = new AirQualityApi();
-        AirQualityResult airQualityResult = airQualityApi.getStats(117).blockingGet();
-        System.out.println(airQualityResult);
 
         final Subject<Update> updatesSubject = PublishSubject.create();
         final TelegramBot bot = TelegramBotAdapter.build(args[0]);
 
         updatesSubject.forEach(update -> {
-            long stationId = Long.parseLong(update.message().text());
-            airQualityApi.getStats(stationId).subscribe(aqi -> {
-                SendMessage request = new SendMessage(update.message().chat().id(), aqi.toString())
+            try {
+                long stationId = Long.parseLong(update.message().text());
+                airQualityApi.getStats(stationId).subscribe(aqi -> {
+                    SendMessage request = new SendMessage(update.message().chat().id(), formatMessage(aqi))
+                            .parseMode(ParseMode.Markdown)
+                            .disableWebPagePreview(true)
+                            .disableNotification(true)
+                            .replyToMessageId(update.message().messageId());
+                    bot.execute(request);
+                });
+            } catch (NumberFormatException ex) {
+
+                SendMessage request = new SendMessage(update.message().chat().id(), "Podaj numer stacji, np 117")
                         .parseMode(ParseMode.Markdown)
                         .disableWebPagePreview(true)
                         .disableNotification(true)
                         .replyToMessageId(update.message().messageId());
                 bot.execute(request);
-            });
+            }
         });
 
         bot.setUpdatesListener(updates -> {
             updates.forEach(updatesSubject::onNext);
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
+    }
+
+    private static String formatMessage(AirQualityResult airQualityResult) {
+        return  airQualityResult.getStation().getName() + ":\n"
+                + " - Pyłki PM2.5: *" + airQualityResult.getValues().get(PartType.PM25) + " µg/m3*\n"
+                + " - Pyłki PM10: *" + airQualityResult.getValues().get(PartType.PM10) + " µg/m3*\n";
     }
 
 }
