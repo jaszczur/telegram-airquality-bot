f664989c6d99d5a8a6e90b0b7297af24080538a7
Jaszczur <jaszczur20@interia.pl>
2017-02-18 08:57:47 +0100

    very nasty but somehow gets the data

--- src/main/scala/pl/jaszczur/bots/aqi/Main.scala
+++ src/main/scala/pl/jaszczur/bots/aqi/Main.scala
@@ -1,7 +1,28 @@
 package pl.jaszczur.bots.aqi
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+
+import scala.util.{Failure, Success}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("dupa")
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+
+    val stationId = 117
+    val apiUrl = s"http://powietrze.gios.gov.pl/pjp/current/getAQIDetails?id=$stationId&param=AQI"
+     Source.single((HttpRequest(uri = apiUrl), 117))
+      .via(Http().superPool())
+      .map {
+        case (Success(r), stationId) => println(r.entity)
+        case (Failure(ex), stationId) => ex.printStackTrace()
+      }
+        .runForeach { case _ => system.shutdown()}
+
+
   }
 }
