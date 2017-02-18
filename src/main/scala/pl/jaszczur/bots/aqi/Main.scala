f97d1e5473c760a37aad4eb8e238d0042cf95ac0
Jaszczur <jaszczur20@interia.pl>
2017-02-18 10:10:55 +0100

    ale chujnia

--- src/main/scala/pl/jaszczur/bots/aqi/Main.scala
+++ src/main/scala/pl/jaszczur/bots/aqi/Main.scala
@@ -2,26 +2,39 @@ package pl.jaszczur.bots.aqi
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.marshalling.Marshaller
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, ResponseEntity}
+import akka.http.scaladsl.unmarshalling.{PredefinedFromEntityUnmarshallers, Unmarshaller}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
+import scala.util.parsing.json.{JSON, JSONObject}
 import scala.util.{Failure, Success}
 
 object Main {
+
+  def handle(entity: ResponseEntity): Unit = {
+    entity.
+  }
+
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
+    implicit val um: Unmarshaller[HttpEntity, Option[JSONObject]] = {
+      Unmarshaller.byteStringUnmarshaller.mapWithCharset { (data, charset) =>
+        JSON.parseFull(data.toString()).map(_.asInstanceOf[JSONObject])
+      }
+    }
 
     val stationId = 117
     val apiUrl = s"http://powietrze.gios.gov.pl/pjp/current/getAQIDetails?id=$stationId&param=AQI"
-     Source.single((HttpRequest(uri = apiUrl), 117))
+    Source.single((HttpRequest(uri = apiUrl), 117))
       .via(Http().superPool())
-      .map {
-        case (Success(r), stationId) => println(r.entity)
+      .runForeach {
+        case (Success(r), stationId) => handle(r.entity )
         case (Failure(ex), stationId) => ex.printStackTrace()
       }
-        .runForeach { case _ => system.shutdown()}
 
 
   }
