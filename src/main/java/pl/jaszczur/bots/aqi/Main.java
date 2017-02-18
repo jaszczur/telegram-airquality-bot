2928f64f3cc02dfc9cdcc49d02f0d8aad6fa3716
Jaszczur <jaszczur20@interia.pl>
2017-02-18 16:11:12 +0100

    improved laf ;)

--- src/main/java/pl/jaszczur/bots/aqi/Main.java
+++ src/main/java/pl/jaszczur/bots/aqi/Main.java
@@ -3,6 +3,7 @@ package pl.jaszczur.bots.aqi;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -10,8 +11,7 @@ import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
 
 public class Main {
-    private final
-    AirQualityApi airQualityApi = new AirQualityApi();
+    private final AirQualityApi airQualityApi = new AirQualityApi();
     private final TelegramBot bot;
 
     public Main(TelegramBot bot) {
@@ -25,22 +25,11 @@ public class Main {
         updatesSubject.forEach(update -> {
             try {
                 long stationId = Long.parseLong(update.message().text());
-                airQualityApi.getStats(stationId).subscribe(aqi -> {
-                    SendMessage request = new SendMessage(update.message().chat().id(), formatMessage(aqi))
-                            .parseMode(ParseMode.Markdown)
-                            .disableWebPagePreview(true)
-                            .disableNotification(true)
-                            .replyToMessageId(update.message().messageId());
-                    bot.execute(request);
-                });
+                airQualityApi.getStats(stationId).subscribe(
+                        aqi -> sendMessage(update.message().chat(), formatMessage(aqi)),
+                        err -> sendMessage(update.message().chat(), "Coś nie bangla. Chyba podałeś/aś numer stacji z dupy..."));
             } catch (NumberFormatException ex) {
-
-                SendMessage request = new SendMessage(update.message().chat().id(), "Podaj numer stacji, np 117")
-                        .parseMode(ParseMode.Markdown)
-                        .disableWebPagePreview(true)
-                        .disableNotification(true)
-                        .replyToMessageId(update.message().messageId());
-                bot.execute(request);
+                sendMessage(update.message().chat(), "Podaj numer stacji, np 117");
             }
         });
 
@@ -50,10 +39,18 @@ public class Main {
         });
     }
 
+    private void sendMessage(Chat chat, String text) {
+        SendMessage request = new SendMessage(chat.id(), text)
+                .parseMode(ParseMode.Markdown)
+                .disableWebPagePreview(true)
+                .disableNotification(true);
+        bot.execute(request);
+    }
+
     private String formatMessage(AirQualityResult airQualityResult) {
-        return  airQualityResult.getStation().getName() + ":\n"
-                + " - Pyłki PM2.5: *" + airQualityResult.getValues().get(PartType.PM25) + " µg/m3*\n"
-                + " - Pyłki PM10: *" + airQualityResult.getValues().get(PartType.PM10) + " µg/m3*\n";
+        return airQualityResult.getStation().getName() + ":\n"
+                + " - Pyłki PM2.5: *" + airQualityResult.getValues().get(PartType.PM25) + " µg/m³*\n"
+                + " - Pyłki PM10: *" + airQualityResult.getValues().get(PartType.PM10) + " µg/m³*\n";
     }
 
     public static void main(String[] args) {
