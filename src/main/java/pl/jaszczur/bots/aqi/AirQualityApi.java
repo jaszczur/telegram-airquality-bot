ab08c2b64b5245cdfe1a82b89dfb22e1e5543ff9
Jaszczur <jaszczur20@interia.pl>
2017-02-19 08:39:11 +0100

    cache api results

--- src/main/java/pl/jaszczur/bots/aqi/AirQualityApi.java
+++ src/main/java/pl/jaszczur/bots/aqi/AirQualityApi.java
@@ -1,5 +1,8 @@
 package pl.jaszczur.bots.aqi;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
 import io.reactivex.Single;
 import org.glassfish.jersey.client.rx.RxClient;
@@ -12,26 +15,28 @@ import javax.json.JsonValue;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class AirQualityApi {
 
     private final RxClient<RxCompletionStageInvoker> client = RxCompletionStage.newClient();
+    private final LoadingCache<Long, Single<AirQualityResult>> aqResults = CacheBuilder.newBuilder()
+            .expireAfterWrite(15, TimeUnit.MINUTES)
+            .build(new AirQualityDataLoader());
 
     public Single<AirQualityResult> getStats(long stationId) {
-        CompletableFuture<Response> responseCompletionStage = client
-                .target("http://powietrze.gios.gov.pl/pjp/current/getAQIDetails")
-                .queryParam("id", stationId) //117
-                .queryParam("param", "AQI")
-                .request()
-                .rx()
-                .get().toCompletableFuture();
-        return Single.fromFuture(responseCompletionStage)
-                .map((resp) -> resp.readEntity(JsonObject.class))
-                .map(this::parseResult);
 
+        return Single.defer(() -> {
+            try {
+                return aqResults.get(stationId);
+            } catch (ExecutionException e) {
+                return Single.error(e);
+            }
+        });
     }
 
-    private  AirQualityResult parseResult(JsonObject jsonObject) {
+    private AirQualityResult parseResult(JsonObject jsonObject) {
         Station station = new Station(0, jsonObject.getString("stationName"));
         JsonObject jsonValues = jsonObject.getJsonObject("values");
         ImmutableMap.Builder<PartType, Double> valuesBuilder = ImmutableMap.builder();
@@ -39,11 +44,28 @@ public class AirQualityApi {
         for (Map.Entry<String, JsonValue> entry : jsonValues.entrySet()) {
             for (PartType partType : PartType.values()) {
                 if (partType.getApiName().equals(entry.getKey())) {
-                    valuesBuilder.put(partType, ((JsonNumber)entry.getValue()).doubleValue());
+                    valuesBuilder.put(partType, ((JsonNumber) entry.getValue()).doubleValue());
                 }
             }
         }
         return new AirQualityResult(station, valuesBuilder.build());
     }
 
+    private class AirQualityDataLoader extends CacheLoader<Long, Single<AirQualityResult>> {
+
+        @Override
+        public Single<AirQualityResult> load(Long stationId) throws Exception {
+            CompletableFuture<Response> responseCompletionStage = client
+                    .target("http://powietrze.gios.gov.pl/pjp/current/getAQIDetails")
+                    .queryParam("id", stationId) //117
+                    .queryParam("param", "AQI")
+                    .request()
+                    .rx()
+                    .get().toCompletableFuture();
+            return Single.fromFuture(responseCompletionStage)
+                    .map((resp) -> resp.readEntity(JsonObject.class))
+                    .map(AirQualityApi.this::parseResult)
+                    .cache();
+        }
+    }
 }
