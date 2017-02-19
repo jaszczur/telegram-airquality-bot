2beb3dcb20cb43a24eab62850169bb19b6417334
Jaszczur <jaszczur20@interia.pl>
2017-02-19 21:18:27 +0100

    use cache instead of map to store chat states

--- src/main/java/pl/jaszczur/bots/aqi/ChatStates.java
+++ src/main/java/pl/jaszczur/bots/aqi/ChatStates.java
@@ -1,15 +1,25 @@
 package pl.jaszczur.bots.aqi;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Maps;
 import com.pengrad.telegrambot.model.Chat;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class ChatStates {
-    // TODO: change to expiring cache
-    private Map<Long, ChatState> states = Maps.newHashMap();
+    private Cache<Long, ChatState> states = CacheBuilder.newBuilder()
+            .expireAfterAccess(14, TimeUnit.DAYS)
+            .maximumSize(10 * 1024 * 1024)
+            .build();
 
     public ChatState getState(Chat chat) {
-        return states.computeIfAbsent(chat.id(), k -> new ChatState());
+        try {
+            return states.get(chat.id(), ChatState::new);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
