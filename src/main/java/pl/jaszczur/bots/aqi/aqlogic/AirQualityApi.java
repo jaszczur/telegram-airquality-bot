31062a1ef57a9ceb3e8906cd22582581436a6e08
Jaszczur <jaszczur20@interia.pl>
2017-02-19 15:51:05 +0100

    getStations api call

--- src/main/java/pl/jaszczur/bots/aqi/aqlogic/AirQualityApi.java
+++ src/main/java/pl/jaszczur/bots/aqi/aqlogic/AirQualityApi.java
@@ -9,14 +9,17 @@ import org.glassfish.jersey.client.rx.RxClient;
 import org.glassfish.jersey.client.rx.java8.RxCompletionStage;
 import org.glassfish.jersey.client.rx.java8.RxCompletionStageInvoker;
 
+import javax.json.JsonArray;
 import javax.json.JsonNumber;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class AirQualityApi {
 
@@ -24,9 +27,11 @@ public class AirQualityApi {
     private final LoadingCache<Long, Single<AirQualityResult>> aqResults = CacheBuilder.newBuilder()
             .expireAfterWrite(15, TimeUnit.MINUTES)
             .build(new AirQualityDataLoader());
+    private final LoadingCache<Class, Single<Set<Station>>> stations = CacheBuilder.newBuilder()
+            .expireAfterWrite(1, TimeUnit.DAYS)
+            .build(new StationsDataLoader());
 
     public Single<AirQualityResult> getStats(long stationId) {
-
         return Single.defer(() -> {
             try {
                 return aqResults.get(stationId);
@@ -36,17 +41,14 @@ public class AirQualityApi {
         });
     }
 
-    private AirQualityResult parseResult(JsonObject jsonObject) {
-        Station station = new Station(0, jsonObject.getString("stationName"));
-        JsonObject jsonValues = jsonObject.getJsonObject("values");
-        ImmutableMap.Builder<PartType, Double> valuesBuilder = ImmutableMap.builder();
-
-        for (Map.Entry<String, JsonValue> entry : jsonValues.entrySet()) {
-            PartType.getByApiName(entry.getKey())
-                    .ifPresent(partType ->
-                            valuesBuilder.put(partType, ((JsonNumber) entry.getValue()).doubleValue()));
-        }
-        return new AirQualityResult(station, valuesBuilder.build());
+    public Single<Set<Station>> getStations() {
+        return Single.defer(() -> {
+            try {
+                return stations.get(AirQualityApi.class);
+            } catch (ExecutionException e) {
+                return Single.error(e);
+            }
+        });
     }
 
     private class AirQualityDataLoader extends CacheLoader<Long, Single<AirQualityResult>> {
@@ -62,8 +64,47 @@ public class AirQualityApi {
                     .get().toCompletableFuture();
             return Single.fromFuture(responseCompletionStage)
                     .map((resp) -> resp.readEntity(JsonObject.class))
-                    .map(AirQualityApi.this::parseResult)
+                    .map(this::parseResult)
                     .cache();
         }
+
+        private AirQualityResult parseResult(JsonObject jsonObject) {
+            Station station = new Station(0, jsonObject.getString("stationName"));
+            JsonObject jsonValues = jsonObject.getJsonObject("values");
+            ImmutableMap.Builder<PartType, Double> valuesBuilder = ImmutableMap.builder();
+
+            for (Map.Entry<String, JsonValue> entry : jsonValues.entrySet()) {
+                PartType.getByApiName(entry.getKey())
+                        .ifPresent(partType ->
+                                valuesBuilder.put(partType, ((JsonNumber) entry.getValue()).doubleValue()));
+            }
+            return new AirQualityResult(station, valuesBuilder.build());
+        }
     }
+
+    private class StationsDataLoader extends CacheLoader<Class, Single<Set<Station>>> {
+        @Override
+        public Single<Set<Station>> load(Class notUsed) throws Exception {
+            CompletableFuture<Response> responseCompletionStage = client
+                    .target("http://powietrze.gios.gov.pl/pjp/current/getAQIDetailsList")
+                    .queryParam("param", "AQI")
+                    .request()
+                    .rx()
+                    .get().toCompletableFuture();
+            return Single.fromFuture(responseCompletionStage)
+                    .map((resp) -> resp.readEntity(JsonArray.class))
+                    .map(this::parseResult)
+                    .cache();
+        }
+
+        private Set<Station> parseResult(JsonArray jsonArray) {
+            return jsonArray.stream().map(item -> (JsonObject) item)
+                    .map(jsonStation -> new Station(
+                            jsonStation.getJsonNumber("stationId").longValue(),
+                            jsonStation.getString("stationName")))
+                    .collect(Collectors.toSet());
+        }
+    }
+
+
 }
--- src/main/java/pl/jaszczur/bots/aqi/aqlogic/Station.java
+++ src/main/java/pl/jaszczur/bots/aqi/aqlogic/Station.java
@@ -1,5 +1,7 @@
 package pl.jaszczur.bots.aqi.aqlogic;
 
+import java.util.Objects;
+
 public class Station {
     private final long id;
     private final String name;
@@ -23,5 +25,19 @@ public class Station {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Station station = (Station) o;
+        return id == station.id &&
+                Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
