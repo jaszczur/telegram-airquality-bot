15666ed2a812bde06536fbfc96152770fb6d160e
Piotr Jaszczyk <piotr.jaszczyk@nokia.com>
2017-02-20 14:57:21 +0100

    refactor

--- src/main/java/pl/jaszczur/bots/aqi/Main.java
+++ src/main/java/pl/jaszczur/bots/aqi/Main.java
@@ -13,52 +13,55 @@ import pl.jaszczur.bots.aqi.aqlogic.AirQualityIndexProvider;
 import pl.jaszczur.bots.aqi.commands.GetAirQualityCommand;
 import pl.jaszczur.bots.aqi.commands.SetLocationCommand;
 import pl.jaszczur.bots.aqi.commands.StartCommand;
+import pl.jaszczur.bots.aqi.state.ChatStates;
 import pl.jaszczur.bots.aqi.state.Storage;
 
 import java.io.File;
+import java.util.List;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static final String STATE_STORAGE_FILE_PATH = "./chat.state";
+    private static final String STATE_STORAGE_FILE_PATH = "./chat-state.json";
     private final TelegramBot bot;
     private final Storage storage;
 
-    public Main(TelegramBot bot, Storage storage) {
+    private Main(TelegramBot bot, Storage storage) {
         this.bot = bot;
         this.storage = storage;
     }
 
-    public void start() {
-        AirQualityApi airQualityApi = new AirQualityApi();
-
+    private void start() {
         storage.load().subscribe(chatStates -> {
-            final BotHandler botHandler = new BotHandler(chatStates);
-
-            StartCommand startCommand = new StartCommand(chatStates);
-            SetLocationCommand setLocationCommand = new SetLocationCommand(chatStates, airQualityApi);
-            GetAirQualityCommand getAirQualityCommand = new GetAirQualityCommand(airQualityApi, new AirQualityIndexProvider(), chatStates);
-
-            botHandler
-                    .addCommand(startCommand)
-                    .addCommand(setLocationCommand)
-                    .addCommand(getAirQualityCommand);
-
-            bot.setUpdatesListener(updates -> {
-                Flowable.fromIterable(updates)
-                        .map(Update::message)
-                        .flatMap(message -> botHandler.handle(message).toFlowable())
-                        .map(bot::execute)
-                        .subscribe(
-                                msg -> {
-                                    if (msg.isOk())
-                                        logger.debug("Reply sent");
-                                    else
-                                        logger.warn("Error occurred {}: {}", msg.errorCode(), msg.description());
-                                },
-                                err -> logger.error("What a Terrible Failure", err));
-                return UpdatesListener.CONFIRMED_UPDATES_ALL;
-            });
+            final BotHandler botHandler = createBotHandler(chatStates);
+            bot.setUpdatesListener(updates -> processUpdates(botHandler, updates));
         });
+    }
+
+    private int processUpdates(BotHandler botHandler, List<Update> updates) {
+        Flowable.fromIterable(updates)
+                .map(Update::message)
+                .flatMap(message -> botHandler.handle(message).toFlowable())
+                .map(bot::execute)
+                .subscribe(
+                        msg -> {
+                            if (msg.isOk())
+                                logger.debug("Reply sent");
+                            else
+                                logger.warn("Error occurred {}: {}", msg.errorCode(), msg.description());
+                        },
+                        err -> logger.error("What a Terrible Failure", err));
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
+    }
+
+    private BotHandler createBotHandler(ChatStates chatStates) {
+        final AirQualityApi airQualityApi = new AirQualityApi();
+        final BotHandler botHandler = new BotHandler(chatStates);
+        botHandler
+                .addCommand(new StartCommand(chatStates))
+                .addCommand(new SetLocationCommand(chatStates, airQualityApi))
+                .addCommand(new GetAirQualityCommand(airQualityApi, new AirQualityIndexProvider(), chatStates));
+        return botHandler;
     }
 
     public static void main(String[] args) {
